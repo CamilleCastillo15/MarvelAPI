29d0542a1853a3064f1338ff58220a418aef5202
camillecastillo15@gmail.com <camillecastillo@MacBook-Pro-de-Camille.local>
2018-03-08 00:39:38 +0100

    fix:accès aux urls (pb sur la concaténation) les données sont désormais affichées sur la page sous forme de tableau

--- src/MarvelBundle/Controller/DefaultController.php
+++ src/MarvelBundle/Controller/DefaultController.php
@@ -16,17 +16,17 @@ class DefaultController extends Controller
         //your private key 
         //and your public key 
         //(e.g. md5(ts+privateKey+publicKey)
-        $concat = $ts+$apiKeyPrivate+$apiKeyPublic;
+        $concat = (string)$ts.$apiKeyPrivate.$apiKeyPublic;
         $hash = md5($concat);
         
-        $url = "http://gateway.marvel.com/v1/public/comics?ts=".$ts."&apikey=".$apiKeyPublic."&hash=".$hash;
+        $url = "http://gateway.marvel.com/v1/public/characters?ts=".$ts."&apikey=".$apiKeyPublic."&hash=".$hash;
         //$url = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150";
         
         $ch = curl_init();
 
         // configuration des options
         curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch,CURLOPT_HTTPHEADER,array('Content-Type: application/json'));
+        curl_setopt($ch,CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
 
         // exécution de la session
@@ -35,11 +35,27 @@ class DefaultController extends Controller
         // fermeture des ressources
         curl_close($ch);
         /*curl_multi_getcontent($ch);
-
         $xml = simplexml_load_string($test);
         $test = file_get_contents($url);*/
-        var_dump($hash, $result); 
+        
+        /*$data = $this
+                ->get('serializer')
+                ->deserialize($result, 'json', 'array');*/
+        //$result = preg_replace('!s:(\d+):"(.*?)";!e', "'s:'.strlen('$2').':\"$2\";'", $result);
+        //unserialize($result);
+        $result = json_decode($result, true);
+        dump($result);
+        //var_dump($concat, $ch, $result); exit();
         //exit();
         return $this->render('MarvelBundle:Default:index.html.twig');
     }
+    
+    public function useGuzzle(){
+        $data = $request->getContent();
+        
+        /*$author = $this
+                ->get('serializer')
+                ->deserialize($data, 'array', 'json');*/
+    }
+    
 }
